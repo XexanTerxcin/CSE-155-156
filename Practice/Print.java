be215aa72fe40e3e849658eb4ade5e1acad45374
Xexan Terxcin <xexanterxcin@gmail.com>
2025-08-17 21:15:49 +0600

    Add files via upload

---
+++ Practice/Print.java
@@ -0,0 +1,55 @@
+public class Print {
+  public static void main(String[] args) {
+    System.out.println("Hello World 1+1");
+    System.out.println("Hello World 1+1");
+    System.out.println("Hello World 1+1");
+
+    System.out.print("Hello World 2+2");
+    System.out.print("Hello World 2+2");
+    System.out.print("Hello World 2+2");
+
+    System.out.print("Hello World 2+2\n");
+    System.out.print("Hello World 2+2\n");
+    System.out.print("Hello World 2+2\n");
+
+    System.out.printf("Hello World 3+3\n");
+
+    //printing numbers..?
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(50000);
+
+    //Looking for comments..?
+    //Mathmatics isn't just a funny things...!
+    System.out.println(3 + 3);
+    System.out.println(6 * 6);
+    System.out.println(30 - 3);
+    System.out.println(300 / 3);
+    System.out.println(500 % 7);
+    /* I don't know what am I doing..! 
+    But atleast I'm not doing any crime...! */
+
+    String name = "Cayano"; //string..?
+    System.out.println("Hello " + name); //printing that string..?
+
+
+    String firstName = "John ";
+    String lastName = "Doe";
+    String fullName = firstName + lastName; //more string..!
+    System.out.println(fullName);
+
+    int x = 5;
+    int y = 6;
+    System.out.println(x + y); // Print the value of x + y
+
+    //instead of doing these,
+    int p = 5;
+    int q = 6;
+    int r = 50;
+    System.out.println(p + q + r);
+
+    //do these
+    int s = 5, t = 6, u = 50;
+    System.out.println(s + t + u);
+  }
+}
