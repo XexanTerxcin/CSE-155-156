c2e5802a1ab87add9441fe157b71383cf065b317
Xexan Terxcin <xexanterxcin@gmail.com>
2025-08-17 21:39:02 +0600

    Add files via upload

---
+++ Practice/PrintV1.java
@@ -0,0 +1,18 @@
+public class PrintV1 {
+    public static void main(String[] args) {
+        
+        // Ahh dunno why am I writing this comment...But it's a comment...!
+        int x, y, z;
+        x = y = z = 50;
+        System.out.println(x + y + z);
+
+        //Where the hell did this student come from..?
+        String StudentName = "Ronny Rins";
+        long StudentID = 1510013;
+        int Age = 22;
+        char StudentGrade = 'A';
+
+        System.out.println(StudentName + " with Student ID : " + StudentID + 
+        " and " + Age + " years old got " + StudentGrade + " in CSE111 course");
+    }
+}
