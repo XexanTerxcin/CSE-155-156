d0e510a401bc9a90a19259643fe87c5eac19c0de
Xexan Terxcin <xexanterxcin@gmail.com>
2025-08-22 02:53:52 +0600

    Add files via upload

---
+++ Lab 01/ElectricityBillCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ElectricityBillCalculator {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        
+        System.out.println("Enter the number of units consumed : ");
+        double units = x.nextDouble();
+        
+        if (units < 0) {
+            System.out.println("Error : Units cannot be negative");
+        }
+        
+        double billAmount = 0;
+        
+        // First 100 units
+        if (units >= 0 && units <= 100) {
+            billAmount = units*2;
+        }
+        else if (units >= 101 && units <= 300) {
+            billAmount = (units-100)*3 + 100*2;
+        }
+        else if (units >= 301 && units <= 500) {
+            billAmount = (units-300)*5 + 200*3 + 100*2;
+        }
+        else{
+            billAmount = (units-500)*7 + 200*5 + 200*3 + 100*2;
+        }
+
+        System.out.println("Total units consumed: " + units);
+        System.out.println("Total electricity bill: BDT " + billAmount);
+        
+    }
+}
